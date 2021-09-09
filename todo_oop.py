3da7df485f85468ded6d80b96bc4dfed7aff94f3
alianju520 <alianjum520@gmail.com>
2021-09-09 11:59:43 +0500

    Fourth commit

--- todo_oop.py
+++ todo_oop.py
@@ -1,71 +1,117 @@
 from datetime import datetime
 from enum import Enum
-
+import json
 class status(Enum):
     completed = 1
     doing = 2
     incomplete = 3
-    
 
 class Task(): 
-    _data = []
-    _task_dict = {}
-    def __init__(self,id,task_name,creation_date):
-        self.id = id
-        self.task_name = task_name
-        self.creation_date = creation_date
-    def add(self):
-        self._task_dict['id'] = self.id
-        self._task_dict['task'] = self.task_name
-        self._task_dict['creation_date'] = self.creation_date
-        self._data.append(self._task_dict.copy())
-        return self._data
-    def update(self):
-        search_id =  input("Enter the id of task you want to update: ")
-        new_task = input("Enter thre new Task: ")
-        for i in range(len(self._data)):
-            if int(search_id) in self._data[i].values():
-                self._data[i]['task'] = new_task
-                self._data[i]['updated_date'] = self.creation_date 
-            else:
-                print("Value not found in the list")
-        return self._data
-    def delete(self):
-        delete_id = input("which value do you need to delete: ")
-        for i in range(len(self._data)):
-            if int(delete_id) in self._data[i].values():
-                self._data[i].clear()
-            else:
-                print("Value not found in the list")
-        return self._data
-    def mark_task(self):
-        option = input('to mark task chose positive integer:\n1: Completed\n2: Doing\n3: Incomplete ')
-        for i in range(len(self._data)):
-            self._data[i]['status'] = status(int(option))
+    def __init__(self,id,task_name,creation_date,update_date,stat):
+        self.__id = id
+        self.__task_name = task_name
+        self.__creation_date = creation_date
+        self.__update_date = update_date
+        self.___stat = stat
         
-    def display(self):
-        print(self._data)
-        
+    def get_id(self):
+        return self.__id
+    def get_task_name(self):
+        return self.__task_name
+    def get_creation_date(self):
+        return self.__creation_date
+    def get_update_date(self):
+        return self.__update_date
+    def get_stat(self):
+        return self.___stat
+    def set_id(self,id):
+        self.__id = id
+    def set_task_name(self,task_name):
+        self.__task_name = task_name
+    def set_creation_date(self,creation_date):
+        self.__creation_date = creation_date
+    def set_update_date(self,update_date):
+        self.__update_date = update_date
+    def set_stat(self,stat):
+        self.___stat = stat
+    
+def add(obj_list):
+    num_task = int(input("Enter the number of task you want to Enter: "))
+    for i in range(num_task):
+        obj_list.append(Task("0","0","0",None,None))
+    for obj in obj_list:
+        id  = input("Enter the id: ")
+        obj.set_id(id)
+        task = input("Enter the task: ")
+        obj.set_task_name(task)
+        now = datetime.now()
+        time = now.strftime("%d/%m/%Y %H:%M:%S")
+        obj.set_creation_date(time)
+        opt = int(input("Enter the status of the task\n1:Completed\n2:Doing\n3:incomplete\n"))
+        x =status(opt)
+        obj.set_stat(str(x))
+    return obj_list 
+def update(obj_list):
+    upd_id = input("Enter the id you want to update: ")
+    for obj in obj_list:
+        if upd_id == obj.get_id():
+            task = input("Enter the task: ")
+            obj.set_task_name(task)
+            now = datetime.now()
+            time = now.strftime("%d/%m/%Y %H:%M:%S")
+            obj.set_update_date(time)
+            opt = int(input("Enter the status of the task\n1:Completed\n2:Doing\n3:incomplete\n"))
+            x =status(opt)
+            obj.set_stat(str(x))
+        else:
+            print("Value Not Found")
+    return obj_list
+def delete(obj_list):
+    del_id = input("Enter the id you want to delete: ")
+    for i,obj in enumerate(obj_list):
+        if obj.get_id() == del_id:
+            del obj_list[i]
+            break
+    return obj_list
+def save_file(obj_list):
+    file_name = input("Enter the name of the file:")
+    text_file = open(f'{file_name}.json','w+')
+    for i in range(len(obj_list)):
+        text_file.write(json.dumps(obj_list[i].__dict__) + "\n")
+    text_file.close()    
+    
+def show_menu():
+    print("MENU")
+    print("1: Add any task in list")
+    print("2: Update any task from the list")
+    print("3: Delete any task from list")
+    print("4: Print your task list")    
+    print("5: Save file in json format") 
+    
+    
+    value = input("Enter your choice(please the choice should not be in negative value):")
+    return value
 
 
-choice = input("do you want to run program(yes/no): ")
-i = 0
-
-while(choice=='yes'):
-    i = i + 1
-    now = datetime.now()
-    time = now.strftime("%d/%m/%Y %H:%M:%S")
-    new_task = input('Enter the new task:')
-    obj = Task(i,new_task,time)
-    obj.add()
-    option = input('Do you want to update any task(yes/no): ')
-    if option == 'yes': 
-        obj.update()
-    option2 = input('Do you want to delete any task(yes/no): ')
-    if option2 == 'yes': 
-        obj.delete()
-    option3 = input('Do you want to mark any task(yes/no): ')
-    if option3 == 'yes':
-        obj.mark_task() 
-    obj.display()
-    choice = input("do you want to run program(yes/no): ")
+def main():
+    obj_list = []
+    choice =show_menu()
+    while(choice!='8'):
+        if choice > '0' and choice <= '7':
+            if choice == '1':      
+                obj_list = add(obj_list)
+            elif choice == '2':
+                obj_list = update(obj_list)
+            elif choice == '3':
+                obj_list = delete(obj_list)
+            elif choice == '4':
+                for i in range(len(obj_list)):
+                    string = json.dumps(obj_list[i].__dict__)
+                    print(string)
+            elif choice == '5':
+                save_file(obj_list)   
+            choice =show_menu()
+    
+#Python interperter running as main program 
+if __name__ == '__main__':
+    main()
